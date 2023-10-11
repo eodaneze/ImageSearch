78816724f43f8f78f75d1398ae7df89c90dea257
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-10 23:37:27 -0700

    displaying the search result on the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Form } from 'react-bootstrap';
@@ -9,29 +9,33 @@ const API_URL = 'https://api.unsplash.com/search/photos';
 const IMAGES_PER_PAGE = 20;
 
 const App = () => {
-  console.log(import.meta.env.VITE_API_KEY);
+  // console.log(import.meta.env.VITE_API_KEY);
   const searchInput = useRef(null);
 
-  useEffect(() => {
-    const getImages = async() => {
-       try {
-        const result = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`);
+ const[images, setImages] = useState([]);
+ const[totalPages, setTotalPages] = useState(0);
 
-        console.log(result.data);
-       } catch (error) {
-           console.log(error);
-       }
-    }
-    getImages();
-  }, [])
+
+  const fetchImages = async() => {
+    try {
+      const {data} = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`);
+      // console.log(data);
+          setImages(data.results);
+          setTotalPages(data.total_pages);
+     } catch (error) {
+         console.log(error);
+     }
+  }
 
   const handleSearch = (event) => {
      event.preventDefault();
-     console.log(searchInput.current.value);
+     fetchImages();
+    //  console.log(searchInput.current.value);
   }
 
   const handleSelection = (selection) => {
-     searchInput.current.value = selection
+     searchInput.current.value = selection;
+     fetchImages()
   }
   return (
     <div className="container">
@@ -47,6 +51,18 @@ const App = () => {
             <div className='btn text-white' style={{backgroundColor: '#7676d7'}} onClick={() => handleSelection('birds')}>Birds</div>
             <div className='btn text-white' style={{backgroundColor: '#7676d7'}} onClick={() => handleSelection('cats')}>Cats</div>
             <div className='btn text-white' style={{backgroundColor: '#7676d7'}} onClick={() => handleSelection('shoe')}>Shoes</div>
+        </div>
+        <div className="row mt-5">
+            {
+              images.map(image => {
+                  return (
+                    <div className='col-lg-3'>
+
+                      <img key={image.id} src={image.urls.small} alt={images.alt_description} className='img-fluid' style={{height: '300px'}}/>
+                    </div>
+                  )
+              })
+            }
         </div>
     </div>
   )
